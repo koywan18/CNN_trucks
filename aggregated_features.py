534cbc732cf7c14669ce0df3453e1ef062f523b6
Your Namekoywan18 <felixdesrotours@gmail.com>
2023-07-24 20:49:28 +0200

    added the rest of the individual pipeline functions

--- aggregated_features.py
+++ aggregated_features.py
@@ -194,3 +194,108 @@ def compute_speed(converted_interactions, filtration_outlier_threshold=80):
     converted_interactions_copy = converted_interactions_copy.query(f'instantaneous_speed != {np.inf}') #filtration of outliers also applies to inf
 
     return converted_interactions_copy.drop(['deltat', 'deltal'], axis=1)
+
+def select_trips_from_clustering(clustered_interactions):
+    """
+    This function is to be applied after the clustering process on a specific IMSI. It selects the trips as well as the next interaction to compute aggregated statistics.
+    It must be called before deleting the post day switch.
+
+    Parameters:
+    -----------
+    clustered_interactions : pandas.DataFrame
+        A dataframe sorted by timestamps and containing the following columns:
+            - end_absolutetime_ts : datetime64[ns]
+            - updated_label : int
+
+    Returns:
+    --------
+    pandas.DataFrame
+        A filtered dataframe containing the rows corresponding to trips and the first row in each destination subsequent to a trip
+    """
+    mask = (clustered_interactions['updated_label'] == -1) | (clustered_interactions['updated_label'].shift() == -1)
+    return clustered_interactions.loc[mask]
+
+def delete_post_day_switch(clustered_interactions):
+    """
+    This function is to be applied after the clustering process and the separation between trips and destinations on a specific IMSI.
+    It filters out the night duration. Indeed, due to the lack of data from 11pm to 8am, it is not known wether the IMSI is moving or not during the night.
+    Parameters:
+    -----------
+    trips_interactions : pandas.DataFrame
+        A dataframe containing the interactions during trips/destinations for an IMSI, sorted by timestamps,  with the following column:
+            - start_date : datetime64[ns]
+    Returns:
+    --------
+    pandas.DataFrame
+        A filtered dataframe containing the interactions of trips/destinations from which were withdrawn the first one after each night.
+    """
+    mask = (clustered_interactions['start_date'].shift() != clustered_interactions['start_date'])
+    return clustered_interactions.loc[~mask]
+
+def destinations_duration(clustered_interactions):
+    """
+    This function computes the duration of each destination on a clustered dataframe on interactions
+    Parameters:
+    -----------
+    clustered_interactions : pandas.DataFrame
+        A dataframe containing the interactions of an IMSI, sorted by timestamps,  with the following column:
+            - updated_label : int
+            - delta_time : datetime64[ns]; the duration between an interaction and the previous one
+    Returns:
+    --------
+    list
+        A list with the total duration of each destination sorted chronologically.
+    """ 
+    destinations = compute_speed(clustered_interactions).loc[lambda df: df.updated_label > -1].groupby('updated_label')
+    l = []
+    for _,destination_df in destinations:
+        l.append (destination_df.iloc[1:].delta_time.sum())
+    return l
+
+def additional_statistics_pipeline_IMSI(interactions_hashed_imsi, h3_resolution, eps, min_points, time_filtration_max):
+    """
+    This function computes 10 statistics for a given set of interactions of a single IMSI. The function takes as input:
+    - interactions_hashed_imsi: a pandas dataframe containing the interactions of the IMSI
+    - h3_resolution: the resolution of the H3 grid used to compute the number of H3 cells per day
+    - eps: the maximum distance between two samples for them to be considered as part of the same cluster for DBSCAN clustering
+    - min_points: the number of samples in a neighborhood for a point to be considered as a core point for DBSCAN clustering
+    - time_filtration_max: the minimum time difference between two interactions
+    
+    The function returns a list of 10 statistics:
+    - h3_per_day: the average number of H3 cells per day
+    - total_interactions_per_day: the average number of interactions per day
+    - trip_interactions_per_day: the average number of interactions that are part of a trip per day
+    - non_trip_interactions_per_day: the average number of interactions that are not part of a trip per day
+    - mean_speed_trip: the average speed during trips in meters per second
+    - mean_speed_destination: the average speed during destinations in meters per second
+    - duration_trip_per_day: the average duration of trips in seconds per day
+    - duration_destination_per_day: the average duration of destinations in seconds per day
+    - destination_number_per_day: the average number of destinations per day
+    - mean_duration_destination: the average duration of destinations in seconds
+    """
+    cleaned_interacciones = time_filtration(interactions_hashed_imsi, time_filtration_max) #filter out interactions that are to close in time
+    df = perform_clustering(cleaned_interacciones, eps, min_points)
+    if df.updated_label.max() == -1: #return np.nan is an IMSI has no cluster, that is to say no destinations. The IMSI will be filtered out.
+        return np.nan
+    day_number = cleaned_interacciones.groupby('start_date').count().shape[0] #store the number of days 
+    l_stat = [] #initiate the list of 10 statistics that will return the function
+    #general statistics
+    h3_number_per_day = compute_h3_number(df, h3_resolution)/day_number
+    total_interactions = df.shape[0]
+    df_speed = compute_speed(df)
+    df_speed_trips = select_trips_from_clustering(df_speed)
+    trip_interactions = df_speed_trips.shape[0]
+    l_stat += [h3_number_per_day, total_interactions/day_number, trip_interactions/day_number, (total_interactions-trip_interactions)/day_number]
+    #trips statistics
+    clean_speed_trip = delete_post_day_switch(df_speed_trips)
+    mean_speed_trip = clean_speed_trip.instantaneous_speed.mean()
+    duration_trip = clean_speed_trip.delta_time.sum().total_seconds()
+    #destination statistics
+    clean_speed_destination = delete_post_day_switch(df_speed).loc[lambda df: df['updated_label'] > -1]
+    mean_speed_destination = clean_speed_destination.instantaneous_speed.mean()
+    time_destinations_list = destinations_duration(clean_speed_destination.reset_index())
+    destination_number = len(time_destinations_list)
+    duration_destination = pd.Series(time_destinations_list).sum().total_seconds()
+    mean_duration_destination = pd.Series(time_destinations_list).mean().total_seconds()
+    l_stat += [mean_speed_trip, mean_speed_destination, duration_trip/day_number, duration_destination/day_number, destination_number/day_number, mean_duration_destination]
+    return l_stat
